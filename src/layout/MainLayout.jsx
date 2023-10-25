511a40839dbb3ec143baead5d5236d55ca50d62c
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-25 07:37:26 +0600

    add navbar and footer also setup react

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div className="container mx-auto px-4 md:px-8 lg:px-16">
+            <Navbar></Navbar>
+           </div>
+            <div className="container mx-auto min-h-screen px-4 md:px-8 lg:px-16">
+            <Outlet></Outlet>
+           </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
